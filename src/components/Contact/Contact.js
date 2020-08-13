42314bd15e133b0d98f68ce4a16abd9e3cd85076
Katerina <matveevakaterinka@gmail.com>
2020-08-14 00:21:12 +1000

    update Contact Page, fixed email sending

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Contact.css';
-// import ContactForm from '../ContactForm/ContactForm';
+import ContactForm from '../ContactForm/ContactForm';
 import sample2 from './Animation - 4519.mp4';
 import LightSpeed from 'react-reveal/LightSpeed';
 import Spin from 'react-reveal/Spin';
@@ -13,48 +13,41 @@ function Contact() {
       </video>
 
       <div className="Contact-container">
-        <LightSpeed left cascade>
-          <div className="contact-offer">
-            <h4>It's always a pleasure for me to meet new people</h4>
+        <div className="contact-offer">
+          <LightSpeed left cascade>
+            {/* <h4>
+              It's always a pleasure for me<br></br> to meet new people
+            </h4> */}
             <h4>Want to share ideas or has an offer</h4>
-            <h4>I'm happy to have a chat</h4>
+            {/* <h4>Drop me a line</h4> */}
+          </LightSpeed>
+          <ContactForm />
+
+          <div className="contact__links">
+            <Spin>
+              <div className="linkedin">
+                <a
+                  href="https://www.linkedin.com/in/katerina-matveeva/"
+                  target="blank"
+                  title="linkedin"
+                >
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </Spin>
+            <Spin>
+              <div className="github">
+                <a
+                  href="https://github.com/katemat"
+                  target="blank"
+                  title="github"
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </Spin>
           </div>
-        </LightSpeed>
-        {/* <div className="contact-offer">Drop me a line</div> */}
-
-        <div className="contact__links">
-          <Spin>
-            <div className="email">
-              <a href="mailto:katmatveeva6@gmail.com" title="send email">
-                📮
-              </a>
-            </div>
-          </Spin>
-          <Spin>
-            <div className="linkedin">
-              <a
-                href="https://www.linkedin.com/in/katerina-matveeva/"
-                target="blank"
-                title="linkedin"
-              >
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-          </Spin>
-          <Spin>
-            <div className="github">
-              <a
-                href="https://github.com/katemat"
-                target="blank"
-                title="github"
-              >
-                <i className="fab fa-github"></i>
-              </a>
-            </div>
-          </Spin>
         </div>
-
-        {/* <ContactForm /> */}
       </div>
     </div>
   );
